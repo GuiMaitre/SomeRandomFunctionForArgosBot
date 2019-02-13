131b9c783e22e4b764f9e1c2b4c0f61119c6460f
Maitre <guillaume.maitre@student.unamur.be>
2019-02-13 15:44:59 +0100

    Join in the black circle

--- testground.lua
+++ testground.lua
@@ -8,14 +8,15 @@ function init()
     robot.colored_blob_omnidirectional_camera.enable()
 end
 
---moving
+--moving ______________________________________________________________________________________________________________________________________________
 function driveAsCar(forwardSpeed, angularSpeed)
     leftSpeed  = forwardSpeed - angularSpeed
     rightSpeed = forwardSpeed + angularSpeed
     robot.wheels.set_velocity(leftSpeed,rightSpeed)
 end
+-- ______________________________________________________________________________________________________________________________________________
 
---obstacle avoidance
+--obstacle avoidance ______________________________________________________________________________________________________________________________________________
 function avoiding(proxiLeft, proxiRight)
     for i = 1,5 do
         proxiLeft = proxiLeft + robot.proximity[i].value
@@ -46,8 +47,9 @@ function getFloor(floor)
     end
     return floor
 end
+-- ______________________________________________________________________________________________________________________________________________
 
---make the bot wander until it goes over black surface
+--make the bot wander until it goes over black surface _______________________________________________________________________
 function wanderToBlack()
     floor = getFloor(0)
     if floor ~= 0 then
@@ -70,8 +72,9 @@ function wanderToBlack()
         robot.leds.set_single_color(13, "red")
     end
 end
+-- ______________________________________________________________________________________________________________________________________________
 
---debug de la vision pour afficher en log ce qu'il ce passe
+--debug de la vision pour afficher en log ce qu'il ce passe _______________________________________________________________________
 function visionDebug()
     for i = 1, #robot.colored_blob_omnidirectional_camera do
         blob = robot.colored_blob_omnidirectional_camera[i]
@@ -82,7 +85,9 @@ function visionDebug()
         " / green: " .. blob.color.green)
     end
 end
+-- ______________________________________________________________________________________________________________________________________________
 
+-- ______________________________________________________________________________________________________________________________________________
 function joinBlack()
     if #robot.colored_blob_omnidirectional_camera == 0 then
         wanderToBlack()
@@ -94,10 +99,7 @@ function joinBlack()
         for i = 1,#robot.colored_blob_omnidirectional_camera do
             ang = ang + math.deg(robot.colored_blob_omnidirectional_camera[i].angle)
             meanAng = ang/#robot.colored_blob_omnidirectional_camera
-            --  log("angle" .. math.deg(robot.colored_blob_omnidirectional_camera[i].angle))
         end
-        --log("meanAng" .. meanAng)
-
         --If the mean of the angle is less than 20 go foward, else turn
         if (meanAng >= 20) then
             robot.wheels.set_velocity(-1,1)
@@ -108,12 +110,72 @@ function joinBlack()
         end
     end
 end
+-- ______________________________________________________________________________________________________________________________________________
 
---[[ This function is executed at each time step
+--line follow and turn _______________________________________________________________________
+function lineFollow()
+    proxiLeft = 0
+    proxiRight = 0
+    for i = 1,12 do
+        proxiLeft = proxiLeft + robot.proximity[i].value
+    end
+    for i = 13,24 do
+        proxiRight = proxiRight + robot.proximity[i].value
+    end
+
+    if (proxiLeft > 0) and (proxiRight == 0) then
+        --black on left of bot
+        if (robot.motor_ground[1].value == 0) and (robot.motor_ground[2].value == 0) then
+            wanderToBlack()
+
+        --black on front of bot
+        elseif (robot.motor_ground[1].value == 0) and (robot.motor_ground[4].value == 0) then
+            wanderToBlack()
+
+        --black on right of bot
+        elseif (robot.motor_ground[3].value == 0) and (robot.motor_ground[4].value == 0) then
+            driveAsCar(40,35)
+
+        --one motor on black
+        else
+            if (robot.motor_ground[1].value == 0) or (robot.motor_ground[2].value == 0) then
+                driveAsCar(0,0)
+            elseif (robot.motor_ground[3].value == 0) or (robot.motor_ground[4].value == 0) then
+                driveAsCar(40,35)
+            end
+        end
+    elseif (proxiLeft == 0) and (proxiRight > 0) then
+        --black on left of bot
+        if (robot.motor_ground[1].value == 0) and (robot.motor_ground[2].value == 0) then
+            driveAsCar(40,-35)
+        --black on front of bot
+        elseif (robot.motor_ground[1].value == 0) and (robot.motor_ground[4].value == 0) then
+            wanderToBlack()
+        --black on right of bot
+        elseif (robot.motor_ground[3].value == 0) and (robot.motor_ground[4].value == 0) then
+            wanderToBlack()
+        --one motor on black
+        else
+            if (robot.motor_ground[3].value == 0) or (robot.motor_ground[4].value == 0) then
+                driveAsCar(0,0)
+            elseif (robot.motor_ground[1].value == 0) or (robot.motor_ground[2].value == 0) then
+                driveAsCar(40,-35)
+            end
+        end
+    -- SI rien en proxy
+    else
+        wanderToBlack()
+    end
+end
+-- ______________________________________________________________________________________________________________________________________________
+
+
+--[[ This function is executed at each time step _______________________________________________________________________
 It must contain the logic of your controller ]]
 function step()
     -- getMeanDistance
     dist = 0
+    meanDist = 0
     for i = 1,#robot.colored_blob_omnidirectional_camera do
         dist = dist + robot.colored_blob_omnidirectional_camera[i].distance
         meanDist = dist/#robot.colored_blob_omnidirectional_camera
@@ -121,24 +183,29 @@ function step()
     end
 
     --avoid while going in the direction and go around if not on black
-    if (meanDist >= 10) then
+    if (meanDist >= 10) or (meanDist == 0) then
         joinBlack()
     else
         floor = getFloor(0)
         if ( floor == 0 ) then
-            if count < 4 then
+            if count > 4 then
                 count = count + 1
                 robot.wheels.set_velocity(0,0)
                 robot.leds.set_single_color(13, "red")
+            else
+                avoiding(0, 0)
             end
-        elseif (floor < 0) && (floor <= 2) then
-            --chopper l'angle et le faire aller dans le bon sens (line follow) et stopper quand capteur interne ok
-        elseif (floor > 2) then
-            --gauche ou droite et inverse après avec test de distance
+        elseif (floor > 0) and (floor <= 3) then
+            --[[chopper l'angle et le faire aller dans le bon sens (line follow) et
+                stopper quand capteur interne ok]]
+            lineFollow()
+        else
+            --random move
+            driveAsCar(40,35)
         end
     end
 end
-
+-- ______________________________________________________________________________________________________________________________________________
 
 --[[ This function is executed every time you press the 'reset'
 button in the GUI. It is supposed to restore the state
